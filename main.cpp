52dcf9dfd21c2b81a8da663b71f4f116759cfc40
MichaMikoajczyk <>
2021-05-12 09:11:13 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int avokado;
+
+int main() {
+  cout << "podaj liczbe avokado\n";
+  cin >> avokado;
+  cout << "tyle avokado kosztuje " << avokado*1.8 << "zl";
+}
